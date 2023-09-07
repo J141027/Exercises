eb3b3d019ae15ea81ca68f4e18d9bd20d80ce7b9
J141027 <129272649+J141027@users.noreply.github.com>
2023-09-07 10:48:17 +0800

    Create function01.cpp

---
+++ function01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#include "MyHeader.h"
+
+// void showInfo();  //最單純的函式的原型宣告
+
+int main(){
+    showInfo();    
+    showInfo();
+    showInfo();
+
+    return 0;
+}
+
+
